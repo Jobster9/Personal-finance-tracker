7aa93db5e61ee6e0186e41c9bfa82d71cbfd1c53
Joby Else <113983712+Jobster9@users.noreply.github.com>
2025-09-23 20:20:01 +0100

    Account functionality

---
+++ FinanceManager.cpp
@@ -0,0 +1,49 @@
+#include "FinanceManager.h"
+#include <stdexcept>
+#include <chrono>
+
+void FinanceManager::addTransaction(const Transaction &transaction)
+{
+    transactions.push_back(transaction);
+}
+
+void FinanceManager::addAccount(const Account &account)
+{
+    accounts.push_back(account);
+}
+
+Account &FinanceManager::getAccount(const string &name)
+{
+    for (auto &account : accounts)
+    {
+        if (account.getName() == name)
+        {
+            return account;
+        }
+    }
+    throw runtime_error("Account '" + name + "' not found");
+}
+
+void FinanceManager::transfer(const string &fromAccountName, const string &toAccountName, double amount)
+{
+    Account &fromAccount = getAccount(fromAccountName);
+    Account &toAccount = getAccount(toAccountName);
+
+    if (fromAccount.getBalance() < amount)
+    {
+        throw runtime_error("Insufficient funds");
+    }
+
+    fromAccount.withdraw(amount);
+    toAccount.deposit(amount);
+
+    auto now = std::chrono::system_clock::now();
+
+    Transaction withdrawal(amount, "Transfer to " + toAccountName,
+                           now, ExpenseCategory::OTHER);
+    Transaction deposit(amount, "Transfer from " + fromAccountName,
+                        now, IncomeCategory::OTHER);
+
+    transactions.push_back(withdrawal);
+    transactions.push_back(deposit);
+}
--- FinanceManager.h
+++ FinanceManager.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include "Transaction.h"
+#include "Account.h"
 
 using namespace std;
 
@@ -11,7 +12,15 @@ class FinanceManager
 public:
     FinanceManager();
 
+    void addTransaction(const Transaction &transaction);
+
+    void addAccount(const Account &account);
+    Account &getAccount(const string &name);
+
+    void transfer(const string &fromAccountName, const string &toAccountName, double amount);
+
 private:
+    vector<Account> accounts;
     vector<Transaction> transactions;
 };
 
